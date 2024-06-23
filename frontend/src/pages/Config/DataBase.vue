eaa1209087e0f45a3963498db2354fc7047ebc04
suki-9 <suki.music2004@gmail.com>
2024-06-24 03:22:27 +0900

    DB削除時に確認のModalが出るように

--- frontend/src/pages/Config/DataBase.vue
+++ frontend/src/pages/Config/DataBase.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { IndexedDB } from '@/scripts/indexedDB';
+import { ref } from 'vue';
 
 const db = new IndexedDB();
-const deleteDB = () => db.delete();
+const showModal = ref<boolean>(false);
 </script>
 
 <template>
@@ -15,12 +16,23 @@ const deleteDB = () => db.delete();
     <M-Divider />
 
     <div>
-      <M-Button type="text"  @click="deleteDB">データの削除</M-Button>
+      <M-Button type="text"  @click="showModal = true">データの削除</M-Button>
       <M-Button type="text" >データのエクスポート (TODO)</M-Button>
       <M-Button type="text" >データの引き継ぎ (TODO)</M-Button>
     </div>
 
   </div>
+
+  <M-Modal v-model:show="showModal" :class="$style.modal">
+    <h2>
+      <M-Icon name="delete"/><p>データベースを削除</p>
+    </h2>
+    <p>全ての削除しますか？</p>
+    <div>
+      <M-Button type="text" @click="showModal = false">Cancel</M-Button>
+      <M-Button type="text" @click="db.delete(), showModal = false">Ok</M-Button>
+    </div>
+  </M-Modal>
 </template>
 
 <style module lang="scss">
@@ -32,5 +44,9 @@ const deleteDB = () => db.delete();
   & > div > * {
     font-size: 1.2em;
   }
+}
+
+.modal {
+  @extend .deleteModal;
 }
 </style>
